29845cb24cb9ea88a3cd102a12d638779fe59523
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-03-09 20:04:19 +0000

    axios installed, user GET request occurs on App mount

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    userData: {},
+    followers: [],
+  }
   
+  componentDidMount() {
+    axios.get('https://api.github.com/users/andromedis')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          userData: res.data
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
   render() {
     return <div>
 
